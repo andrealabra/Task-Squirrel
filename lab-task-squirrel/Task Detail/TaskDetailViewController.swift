df3337c1e26804325adfcc1555e4428327fde899
Andrea Labra Orozco <andrealabra21@gmail.com>
2023-03-14 18:07:15 -0700

    Done ✅

--- lab-task-squirrel/Task Detail/TaskDetailViewController.swift
+++ lab-task-squirrel/Task Detail/TaskDetailViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  TaskDetailViewController.swift
 //  lab-task-squirrel
@@ -7,9 +8,7 @@
 
 import UIKit
 import MapKit
-import Photos
-
-// TODO: Import PhotosUI
+import PhotosUI
 
 class TaskDetailViewController: UIViewController {
 
@@ -27,9 +26,10 @@ class TaskDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: Register custom annotation view
+        
+        mapView.register(TaskAnnotationView.self, forAnnotationViewWithReuseIdentifier: TaskAnnotationView.identifier)
 
-        // TODO: Set mapView delegate
+        mapView.delegate = self
 
         // UI Candy
         mapView.layer.cornerRadius = 12
@@ -59,10 +59,6 @@ class TaskDetailViewController: UIViewController {
     }
 
     @IBAction func didTapAttachPhotoButton(_ sender: Any) {
-        // TODO: Check and/or request photo library access authorization.
-
-        // If authorized, show photo picker, otherwise request authorization.
-        // If authorization denied, show alert with option to go to settings to update authorization.
         if PHPhotoLibrary.authorizationStatus(for: .readWrite) != .authorized {
             // Request photo library access
             PHPhotoLibrary.requestAuthorization(for: .readWrite) { [weak self] status in
@@ -85,24 +81,67 @@ class TaskDetailViewController: UIViewController {
             // Show photo picker
             presentImagePicker()
         }
-        
+
     }
 
     private func presentImagePicker() {
-        // TODO: Create, configure and present image picker.
+        var config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+
+        // Set the filter to only show images as options (i.e. no videos, etc.).
+        config.filter = .images
+
+        // Request the original file format. Fastest method as it avoids transcoding.
+        config.preferredAssetRepresentationMode = .current
+
+        // Only allow 1 image to be selected at a time.
+        config.selectionLimit = 1
+
+        // Instantiate a picker, passing in the configuration.
+        let picker = PHPickerViewController(configuration: config)
+
+        // Set the picker delegate so we can receive whatever image the user picks.
+        picker.delegate = self
+
+        // Present the picker.
+        present(picker, animated: true)
 
     }
 
     func updateMapView() {
-        // TODO: Set map viewing region and scale
+        guard let imageLocation = task.imageLocation else { return }
 
-        // TODO: Add annotation to map view
+        // Get the coordinate from the image location. This is the latitude / longitude of the location.
+        // https://developer.apple.com/documentation/mapkit/mkmapview
+        let coordinate = imageLocation.coordinate
+
+        // Set the map view's region based on the coordinate of the image.
+        // The span represents the maps's "zoom level". A smaller value yields a more "zoomed in" map area, while a larger value is more "zoomed out".
+        let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
     }
 }
 
-// TODO: Conform to PHPickerViewControllerDelegate + implement required method(s)
 
-// TODO: Conform to MKMapKitDelegate + implement mapView(_:viewFor:) delegate method.
+extension TaskDetailViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        // Dequeue the annotation view for the specified reuse identifier and annotation.
+        // Cast the dequeued annotation view to your specific custom annotation view class, `TaskAnnotationView`
+        // 💡 This is very similar to how we get and prepare cells for use in table views.
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: TaskAnnotationView.identifier, for: annotation) as? TaskAnnotationView else {
+            fatalError("Unable to dequeue TaskAnnotationView")
+        }
+
+        // Configure the annotation view, passing in the task's image.
+        annotationView.configure(with: task.image)
+        return annotationView
+    }
+
+}
 
 // Helper methods to present various alerts
 extension TaskDetailViewController {
@@ -141,4 +180,55 @@ extension TaskDetailViewController {
 
         present(alertController, animated: true)
     }
+}
+extension TaskDetailViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+
+        // Get the selected image asset (we can grab the 1st item in the array since we only allowed a selection limit of 1)
+        let result = results.first
+
+        // Get image location
+        // PHAsset contains metadata about an image or video (ex. location, size, etc.)
+        guard let assetId = result?.assetIdentifier,
+              let location = PHAsset.fetchAssets(withLocalIdentifiers: [assetId], options: nil).firstObject?.location else {
+            return
+        }
+        print("📍 Image location coordinate: \(location.coordinate)")
+        
+        guard let provider = result?.itemProvider,
+              // Make sure the provider can load a UIImage
+              provider.canLoadObject(ofClass: UIImage.self) else { return }
+
+        // Load a UIImage from the provider
+        provider.loadObject(ofClass: UIImage.self) { [weak self] object, error in
+
+            // Handle any errors
+            if let error = error {
+              DispatchQueue.main.async { [weak self] in self?.showAlert(for:error) }
+            
+            }
+
+            // Make sure we can cast the returned object to a UIImage
+            guard let image = object as? UIImage else { return }
+
+            print("🌉 We have an image!")
+
+            // UI updates should be done on main thread, hence the use of `DispatchQueue.main.async`
+            DispatchQueue.main.async { [weak self] in
+
+                // Set the picked image and location on the task
+                self?.task.set(image, with: location)
+
+                // Update the UI since we've updated the task
+                self?.updateUI()
+
+                // Update the map view since we now have an image an location
+                self?.updateMapView()
+            }
+        }
+    }
+    
+    
+
 }
--- lab-task-squirrel/Task List/TaskListViewController.swift
+++ lab-task-squirrel/Task List/TaskListViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import PhotosUI
 
 class TaskListViewController: UIViewController {
 
